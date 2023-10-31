599a2373a7d293c22943e6257fbbd4b824ff0e36
Melike İsbir <melikeisbr@gmail.com>
2023-10-31 22:46:03 +0300

    context class

--- EntityLayer/Concrete/Service.cs
+++ EntityLayer/Concrete/Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class Service
     {
+        [Key]
         public int ServiceID { get; set; }
         public string Title { get; set; }
         public string ImageUrl { get; set; }
