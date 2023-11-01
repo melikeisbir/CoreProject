f47d94a6b2925ab0a4d978715cee77002c627cac
Melike İsbir <melikeisbr@gmail.com>
2023-11-01 22:18:22 +0300

    generic service, abstract services, manager class

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T TGetByID(int id); //dışarıdan t int türünde id parametresi alacak silme güncelleme işlemleri öncesinde çağrılıyor
+    }
+}
