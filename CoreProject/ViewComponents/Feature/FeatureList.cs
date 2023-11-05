ff0542f918095e034860e2d5ab50d36d996afea9
Melike İsbir <melikeisbr@gmail.com>
2023-11-05 22:32:07 +0300

    ViewComponents

---
+++ CoreProject/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreProject.ViewComponents.Feature
+{
+    public class FeatureList: ViewComponent  //Shared'da oluşturduğumuz componentsde klasör
+                                             //oluştururken viewcomponentsde oluşturduğumuz adla aynı olamı
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
