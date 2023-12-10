81aad3bbae2024529b08ad10b4623f8a4c560d7c
Melike İsbir <melikeisbr@gmail.com>
2023-12-10 14:24:09 +0300

    Admin Theme, Layout, Partial SideBar

---
+++ CoreProject/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
