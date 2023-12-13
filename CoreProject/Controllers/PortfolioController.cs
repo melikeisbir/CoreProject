d425fc825dff9e11742a3ab67ab2589a4df0bd8d
Melike İsbir <melikeisbr@gmail.com>
2023-12-13 21:28:40 +0300

    portfolio list, portfolio entity edits

---
+++ CoreProject/Controllers/PortfolioController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
