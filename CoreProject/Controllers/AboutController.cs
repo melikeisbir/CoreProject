554e64bbe53f6292c68f2d265eafa4cfbfc64b80
Melike İsbir <melikeisbr@gmail.com>
2023-12-17 19:27:32 +0300

    editing about

---
+++ CoreProject/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = " Hakkımda";
+            ViewBag.v3 = " Hakkımda Sayfası";
+            var values = aboutManager.TGetByID(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            aboutManager.TUpdate(about);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
