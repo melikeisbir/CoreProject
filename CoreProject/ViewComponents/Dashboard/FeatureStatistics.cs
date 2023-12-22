4342497e52f0a8c67ed3a485891fda7561435726
Melike İsbir <melikeisbr@gmail.com>
2023-12-22 19:56:10 +0300

    writing the statistics

--- CoreProject/ViewComponents/Dashboard/FeatureStatistics.cs
+++ CoreProject/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,15 +1,20 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreProject.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
-       // FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            //var values = featureManager.TGetList();
+            ViewBag.v1 = c.Skills.Count(); //yetenekler listelensşn
+            ViewBag.v2 = c.Messages.Where(x => x.Status == false).Count(); //okunmamış mesajlar
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.Experiences.Count();
             return View();
         }
     }
