779f0d5f99c85ba5761316ea60169c15ce0ed492
Melike İsbir <melikeisbr@gmail.com>
2023-12-26 17:32:27 +0300

    ToDoList Entity

---
+++ BusinessLayer/Concrete/ToDoListManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ToDoListManager : IToDoListService
+    {
+        IToDoListDal _todolistDal;
+
+        public ToDoListManager(IToDoListDal todolistDal)
+        {
+            _todolistDal = todolistDal;
+        }
+
+        public void TAdd(ToDoList t)
+        {
+            _todolistDal.Insert(t);
+        }
+
+        public void TDelete(ToDoList t)
+        {
+            _todolistDal.Delete(t);
+        }
+
+        public ToDoList TGetByID(int id)
+        {
+            return _todolistDal.GetByID(id);
+        }
+
+        public List<ToDoList> TGetList()
+        {
+            return _todolistDal.GetList();
+        }
+
+        public void TUpdate(ToDoList t)
+        {
+            _todolistDal.Update(t);
+        }
+    }
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -26,6 +26,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserMessage> UserMessages { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
 
     }
 }
