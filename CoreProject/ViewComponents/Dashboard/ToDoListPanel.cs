f0f6b8941baffe90a982ed33f451a37caa2fdb60
Melike İsbir <melikeisbr@gmail.com>
2023-12-26 17:49:59 +0300

    listing ToDoList

---
+++ CoreProject/ViewComponents/Dashboard/ToDoListPanel.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Dashboard
+{
+    public class ToDoListPanel : ViewComponent
+    {
+        ToDoListManager todolistManager = new ToDoListManager(new EfToDoListDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = todolistManager.TGetList();
+            return View(values);
+        }
+    }
+}
