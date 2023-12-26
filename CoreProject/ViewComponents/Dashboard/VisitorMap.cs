2da5df4552189a589c37917459786fb68438377b
Melike İsbir <melikeisbr@gmail.com>
2023-12-26 17:58:21 +0300

    dashboard VisitorMap

---
+++ CoreProject/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Dashboard
+{
+    public class VisitorMap : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
