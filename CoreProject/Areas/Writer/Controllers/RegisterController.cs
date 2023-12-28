29586276cb1709392d16e22e1601b9f6fdbe8bdb
Melike İsbir <melikeisbr@gmail.com>
2023-12-28 21:04:32 +0300

    register form, identity libraries

---
+++ CoreProject/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string p)
+        {
+            return View();
+        }
+    }
+}
