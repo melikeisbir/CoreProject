7c7dfdc0f2948debe9edf9d11aac7ee246bb21d1
Melike İsbir <melikeisbr@gmail.com>
2023-12-30 21:43:23 +0300

    register backend

--- CoreProject/Areas/Writer/Controllers/RegisterController.cs
+++ CoreProject/Areas/Writer/Controllers/RegisterController.cs
@@ -1,23 +1,54 @@
-﻿using CoreProject.Areas.Writer.Models;
+﻿using BusinessLayer.Concrete;
+using CoreProject.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CoreProject.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class RegisterController : Controller
     {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(UserRegisterViewModel p)
+        public async Task<IActionResult> Index(UserRegisterViewModel p)
         {
             if (ModelState.IsValid) //model geçerliyse
             {
-              
+                WriterUser w = new WriterUser()
+                {
+                    Name = p.UserName,
+                    Surname = p.Surname,
+                    Email = p.Mail,
+                    UserName = p.UserName,
+                    ImageUrl = p.ImageUrl
+                };
+                var result = await _userManager.CreateAsync(w, p.Password); //hesap oluşturma işlemi
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach( var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
             }
+
             return View();
         }
     }
--- CoreProject/Areas/Writer/Models/UserRegisterViewModel.cs
+++ CoreProject/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -4,7 +4,16 @@ namespace CoreProject.Areas.Writer.Models
 {
     public class UserRegisterViewModel
     {
+
+        [Required(ErrorMessage = "Lütfen Adınızı Girin")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Soyadını Girin")]
+        public string Surname { get; set; }
+
         [Required(ErrorMessage = "Lütfen Kullanıcı Adını Girin")]
+        public string ImageUrl { get; set; }
+        [Required(ErrorMessage = "Lütfen Resim Url Değeri Girin")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "Lütfen Şifreyi Girin")]
         public string Password { get; set; }
