95fcd00a5cfc3fc3911554433f8681e703f41321
Melike İsbir <melikeisbr@gmail.com>
2024-01-02 19:49:49 +0300

    login theme

---
+++ CoreProject/Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreProject/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreProject.Areas.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage= "Kullanıcı adını giriniz!")]
+        public string Username { get; set; }
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifreyi giriniz!")]
+        public string Password { get; set; }
+    }
+}
