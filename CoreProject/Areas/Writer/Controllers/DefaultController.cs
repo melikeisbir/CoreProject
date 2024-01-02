7e39821452c9e1e87136b9823f14199dd63c70a0
Melike İsbir <melikeisbr@gmail.com>
2024-01-02 20:13:05 +0300

    login process

--- CoreProject/Areas/Writer/Controllers/DefaultController.cs
+++ CoreProject/Areas/Writer/Controllers/DefaultController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.Areas.Writer.Controllers
 {
     [Area("Writer")] //çalışacağımız areanın adının belirtmeliyiz
+    [Authorize]
     public class DefaultController : Controller
     {
         public IActionResult Index()
--- CoreProject/Areas/Writer/Controllers/LoginController.cs
+++ CoreProject/Areas/Writer/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
-﻿using EntityLayer.Concrete;
+﻿using CoreProject.Areas.Writer.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CoreProject.Areas.Writer.Controllers
 {
@@ -13,9 +15,26 @@ namespace CoreProject.Areas.Writer.Controllers
         {
             _signInManager = signInManager;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, true, true); //login işleminin kontrolü, true= kullanııcı adını hatırlasın
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Default");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı kullanıcı adı veya şifre");
+                }
+            }
             return View();
         }
     }
