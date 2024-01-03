1d0877df773611bc44933f26c5dd45ca8e7da9f5
Melike İsbir <melikeisbr@gmail.com>
2024-01-03 20:02:17 +0300

    announcement table

--- CoreProject/Areas/Writer/Controllers/DefaultController.cs
+++ CoreProject/Areas/Writer/Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.Areas.Writer.Controllers
@@ -7,9 +9,11 @@ namespace CoreProject.Areas.Writer.Controllers
     [Authorize]
     public class DefaultController : Controller
     {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
         public IActionResult Index()
         {
-            return View();
+            var values = announcementManager.TGetList();
+            return View(values);
         }
     }
 }
