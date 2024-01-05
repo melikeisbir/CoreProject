7379c5af0b4159d440114ec851196de6915bc6ef
Melike İsbir <melikeisbr@gmail.com>
2024-01-05 19:57:49 +0300

    announcement details

--- CoreProject/Areas/Writer/Controllers/DefaultController.cs
+++ CoreProject/Areas/Writer/Controllers/DefaultController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,12 @@ namespace CoreProject.Areas.Writer.Controllers
         {
             var values = announcementManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AnnouncementDetails(int id)
+        {
+            Announcement announcement = announcementManager.TGetByID(id);
+            return View(announcement);
         }
     }
 }
