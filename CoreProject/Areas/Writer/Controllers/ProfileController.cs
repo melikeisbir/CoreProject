fa6421fe8ca7fc94adcdafe055800adf5574916c
Melike İsbir <melikeisbr@gmail.com>
2024-01-05 20:16:37 +0300

    retrieving information with profile user manager

--- CoreProject/Areas/Writer/Controllers/ProfileController.cs
+++ CoreProject/Areas/Writer/Controllers/ProfileController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CoreProject.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager; //sisteme otantike olan kullanıcnın bilgisi için
+
+        public ProfileController(UserManager<WriterUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name); //sisteme otantike olan kullanıcnın bilgisi için
+            return View(values); 
         }
     }
 }
