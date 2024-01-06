9bc1ee2fd882022b33c27a5a909ad630bd3d114e
Melike İsbir <melikeisbr@gmail.com>
2024-01-06 21:25:36 +0300

    user edit view model

---
+++ CoreProject/Areas/Writer/Models/UserEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CoreProject.Areas.Writer.Models
+{
+    public class UserEditViewModel //güncellenecek alanlar
+    {
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public string PictureURL { get; set; }
+        public IFormFile Picture { get; set; } //dosya yollu
+    }
+}
