9c1b3f797dd854e6a28d8c84650051bd6b456208
Melike İsbir <melikeisbr@gmail.com>
2024-01-08 15:54:12 +0300

    retrieve the name and surname information of the logged-in user

--- CoreProject/Areas/Writer/Controllers/DashboardController.cs
+++ CoreProject/Areas/Writer/Controllers/DashboardController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CoreProject.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + "" + values.Surname;
             return View();
         }
     }
