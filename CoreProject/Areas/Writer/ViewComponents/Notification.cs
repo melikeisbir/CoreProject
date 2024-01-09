949c2c326bc7dcf2d2933da0ff992cc483750085
Melike İsbir <melikeisbr@gmail.com>
2024-01-09 20:49:18 +0300

    layout notification setting

---
+++ CoreProject/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Areas.Writer.ViewComponents
+{
+    public class Notification : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
