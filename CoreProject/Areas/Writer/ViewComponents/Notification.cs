b3e65594c9d6459ac07901bb4b246fb023023dde
Melike İsbir <melikeisbr@gmail.com>
2024-01-10 16:54:31 +0300

    listing notifications

--- CoreProject/Areas/Writer/ViewComponents/Notification.cs
+++ CoreProject/Areas/Writer/ViewComponents/Notification.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreProject.Areas.Writer.ViewComponents
 {
     public class Notification : ViewComponent
     {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = announcementManager.TGetList().Take(5).ToList(); // ilk 5 bildirim
+            return View(values);
         }
     }
 }
