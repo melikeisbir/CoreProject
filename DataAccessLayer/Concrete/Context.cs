3ba307c0d0908384548b34ab83e461b45d561c43
Melike İsbir <melikeisbr@gmail.com>
2024-01-10 17:22:06 +0300

    writer message table

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -30,5 +30,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<ToDoList> ToDoLists { get; set; }
         public DbSet<Test1> Test1s { get; set; }
         public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<WriterMessage> WriterMessages { get; set; }
     }
 }
--- DataAccessLayer/Migrations/20240110141935_mig001.Designer.cs
+++ DataAccessLayer/Migrations/20240110141935_mig001.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240110141935_mig001")]
+    partial class mig001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -409,11 +411,11 @@ namespace DataAccessLayer.Migrations
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
+                    b.Property<string>("Content")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<DateTime>("Date")
                         .HasColumnType("datetime2");
-
-                    b.Property<string>("MessageContent")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Receiver")
                         .HasColumnType("nvarchar(max)");
