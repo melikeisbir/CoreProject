64acf96c76943969328afe4841722e1f88736892
Melike İsbir <melikeisbr@gmail.com>
2024-01-11 13:40:14 +0300

    message layer operations, Linq expression filter

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace DataAccessLayer.Abstract
         void Update(T t);
         List<T> GetList();
         T GetByID(int id); //idye göre getirme
+        List<T> GetbyFilter(Expression<Func<T, bool>> filter);
     }
 }
