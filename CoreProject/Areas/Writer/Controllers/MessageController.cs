a92fc1c6777defb89a75667b593810dc87cb0071
Melike İsbir <melikeisbr@gmail.com>
2024-01-12 14:04:22 +0300

    listing sender and receiver messages

--- CoreProject/Areas/Writer/Controllers/MessageController.cs
+++ CoreProject/Areas/Writer/Controllers/MessageController.cs
@@ -18,11 +18,18 @@ namespace CoreProject.Areas.Writer.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index(string p)
+        public async Task<IActionResult> ReceiverMessage(string p) //alıcısı olduğumuz mesajlar
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             p = values.Email;
             var messageList = writerMessageManager.GetListReceiverMessage(p);
+            return View(messageList);
+        }
+        public async Task<IActionResult> SenderMessage(string p) //göndericisi olduğumuz mesajlar
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+            var messageList = writerMessageManager.GetListSenderMessage(p);
             return View(messageList);
         }
     }
