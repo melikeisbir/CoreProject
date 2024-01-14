92cb629ad7a6be05efec98b66caf3b6728bacef5
Melike İsbir <melikeisbr@gmail.com>
2024-01-14 13:30:29 +0300

    details of the sent message

--- CoreProject/Areas/Writer/Controllers/MessageController.cs
+++ CoreProject/Areas/Writer/Controllers/MessageController.cs
@@ -32,8 +32,12 @@ namespace CoreProject.Areas.Writer.Controllers
             var messageList = writerMessageManager.GetListSenderMessage(p);
             return View(messageList);
         }
-        [HttpGet]
         public IActionResult MessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetByID(id);
+            return View(writerMessage);
+        }
+        public IActionResult ReceiverMessageDetails(int id)
         {
             WriterMessage writerMessage = writerMessageManager.TGetByID(id);
             return View(writerMessage);
