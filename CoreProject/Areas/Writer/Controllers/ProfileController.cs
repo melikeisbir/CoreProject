dddcc47be92e29977520cf4c318ed8c428309a67
Melike İsbir <melikeisbr@gmail.com>
2024-01-15 19:42:59 +0300

    password update

--- CoreProject/Areas/Writer/Controllers/ProfileController.cs
+++ CoreProject/Areas/Writer/Controllers/ProfileController.cs
@@ -43,6 +43,7 @@ namespace CoreProject.Areas.Writer.Controllers
             }
             user.Name = p.Name;
             user.Surname = p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
             var result = await _userManager.UpdateAsync(user);
 
             if (result.Succeeded)
