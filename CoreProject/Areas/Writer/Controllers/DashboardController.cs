bafad23e04f7dcc2a2cadc025bbb31252cc81a46
Melike İsbir <melikeisbr@gmail.com>
2024-01-15 19:34:40 +0300

    dashboard update

--- CoreProject/Areas/Writer/Controllers/DashboardController.cs
+++ CoreProject/Areas/Writer/Controllers/DashboardController.cs
@@ -31,9 +31,9 @@ namespace CoreProject.Areas.Writer.Controllers
 
             //statistics
             Context c = new Context();
-            ViewBag.v1 = 0;
+            ViewBag.v1 = c.WriterMessages.Where(x=>x.Receiver==values.Email).Count();
             ViewBag.v2 = c.Announcements.Count();
-            ViewBag.v3 = 0;
+            ViewBag.v3 = c.Users.Count();
             ViewBag.v4 = c.Skills.Count();
 
 
