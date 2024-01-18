4c22cab3d22221d62152bd5d1f7d2391ee4aca29
Melike İsbir <melikeisbr@gmail.com>
2024-01-18 13:44:03 +0300

    Contact Admin

---
+++ CoreProject/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IActionResult Index()
+        {
+            var values = messageManager.TGetList();
+            return View(values);
+        }
+    }
+}
