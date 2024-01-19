f2e2889893e37d96ffb52512d2cdda91925e1e5c
Melike İsbir <melikeisbr@gmail.com>
2024-01-19 14:30:29 +0300

    admin receiver message

---
+++ CoreProject/Controllers/AdminMessageController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class AdminMessageController : Controller //writer bölümündeki mesajların admin tarafında da gözükmesi için
+    {
+        WriterMessageManager WriterMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList() //gelen kutusu - alıcısı olduğumuz mesajlar
+        {
+            string p;
+            p = "admin@gmail.com"; //admine mesaj atılacak
+            var values = WriterMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList() //giden kutusu - göndericisi olduğumuz mesajlar
+        {
+            string p;
+            p = "admin@gmail.com";
+            var values = WriterMessageManager.GetListSenderMessage(p);
+            return View(values);
+        }
+    }
+}
