876272dc60abf182e6b563f05c499036f43bf0bd
Melike İsbir <melikeisbr@gmail.com>
2024-01-21 19:19:33 +0300

    sending admin messages

--- CoreProject/Controllers/AdminMessageController.cs
+++ CoreProject/Controllers/AdminMessageController.cs
@@ -1,35 +1,51 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace CoreProject.Controllers
 {
     public class AdminMessageController : Controller //writer bölümündeki mesajların admin tarafında da gözükmesi için
     {
-        WriterMessageManager WriterMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IActionResult ReceiverMessageList() //gelen kutusu - alıcısı olduğumuz mesajlar
         {
             string p;
             p = "admin@gmail.com"; //admine mesaj atılacak
-            var values = WriterMessageManager.GetListReceiverMessage(p);
+            var values = writerMessageManager.GetListReceiverMessage(p);
             return View(values);
         }
         public IActionResult SenderMessageList() //giden kutusu - göndericisi olduğumuz mesajlar
         {
             string p;
             p = "admin@gmail.com";
-            var values = WriterMessageManager.GetListSenderMessage(p);
+            var values = writerMessageManager.GetListSenderMessage(p);
             return View(values);
         }
         public IActionResult AdminMessageDetails(int id)
         {
-            var values = WriterMessageManager.TGetByID(id);
+            var values = writerMessageManager.TGetByID(id);
             return View(values);
         }
         public IActionResult AdminMessageDelete(int id)
         {
-            var values = WriterMessageManager.TGetByID(id);
-            WriterMessageManager.TDelete(values);
+            var values = writerMessageManager.TGetByID(id);
+            writerMessageManager.TDelete(values);
+            return RedirectToAction("SenderMessageList");
+        }
+        [HttpGet]
+        public IActionResult AdminMessageSend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdminMessageSend(WriterMessage p)
+        {
+            p.Sender = "admin@gmail.com";
+            p.SenderName = "Admin";
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            writerMessageManager.TAdd(p);
             return RedirectToAction("SenderMessageList");
         }
     }
