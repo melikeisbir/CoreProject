7f0a6e57f8932a4c1b1c98798db4599bf0c92779
Melike İsbir <melikeisbr@gmail.com>
2024-01-24 18:31:56 +0300

    testimonial index

---
+++ CoreProject/Controllers/TestimonialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
