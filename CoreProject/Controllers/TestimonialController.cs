e38837b3a705fba50e4a6b3c7b8e515df54b1740
Melike İsbir <melikeisbr@gmail.com>
2024-01-24 18:56:41 +0300

    testimonial details

--- CoreProject/Controllers/TestimonialController.cs
+++ CoreProject/Controllers/TestimonialController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.Controllers
 {
     public class TestimonialController : Controller
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
         public IActionResult Index()
         {
-            return View();
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            testimonialManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TUpdate(testimonial);
+            return RedirectToAction("Index");
         }
     }
 }
