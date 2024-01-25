0f88338f4f133dc4076413edb683d665d4fbbf95
Melike İsbir <melikeisbr@gmail.com>
2024-01-25 19:04:31 +0300

    socialmedia operations

---
+++ CoreProject/Controllers/SocialMediaController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia p)
+        {
+            socialMediaManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetByID(id);
+            socialMediaManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(SocialMedia p)
+        {
+            socialMediaManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
