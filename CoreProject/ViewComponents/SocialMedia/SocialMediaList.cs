5f814edfa70ae73fc3d4311d2eebf16f6bcb8ec0
Melike İsbir <melikeisbr@gmail.com>
2024-01-26 21:36:55 +0300

    listing social media in default

--- CoreProject/ViewComponents/SocialMedia/SocialMediaList.cs
+++ CoreProject/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.ViewComponents.SocialMedia
 {
     public class SocialMediaList : ViewComponent
     {
+        SocialMediaManager socialMedia = new SocialMediaManager(new EfSocialMediaDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = socialMedia.TGetList();
+            return View(values);
         }
     }
 }
