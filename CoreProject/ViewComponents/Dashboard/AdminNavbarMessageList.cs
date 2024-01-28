2ae9ab12716602b9ef3ca79f3c90a63051a39be3
Melike İsbir <melikeisbr@gmail.com>
2024-01-28 21:00:16 +0300

    admin navbar message list

---
+++ CoreProject/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
