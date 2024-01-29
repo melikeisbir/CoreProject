de92c2a1d0d10f229c290a708035f9a7c4d9e821
Melike İsbir <melikeisbr@gmail.com>
2024-01-29 23:22:13 +0300

    listing navbar message

--- CoreProject/ViewComponents/Dashboard/AdminNavbarMessageList.cs
+++ CoreProject/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreProject.ViewComponents.Dashboard
 {
     public class AdminNavbarMessageList : ViewComponent
     {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p = "admin@gmail.com"; //admin@gmail.com receiver içerisindeki sadece son 3 mesaj gelecek
+            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x=>x.WriterMessageID).Take(3).ToList();
+            return View(values);
         }
     }
 }
