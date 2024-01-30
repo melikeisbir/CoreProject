f8b7b4d775fb7ba420e540653147685b41b393ce
Melike İsbir <melikeisbr@gmail.com>
2024-01-30 19:18:35 +0300

    adding roles

---
+++ CoreProject/Controllers/AdminLoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
