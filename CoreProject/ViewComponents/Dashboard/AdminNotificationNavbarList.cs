267386f3f6f0f124b02077cc00182559637570de
Melike İsbir <melikeisbr@gmail.com>
2024-01-30 18:49:50 +0300

    listing navbar notification

---
+++ CoreProject/ViewComponents/Dashboard/AdminNotificationNavbarList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Dashboard
+{
+    public class AdminNotificationNavbarList :  ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
