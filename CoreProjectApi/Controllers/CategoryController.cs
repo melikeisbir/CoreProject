78c93fe14176a22960ac12fcf18e73ed0137a37f
Melike İsbir <melikeisbr@gmail.com>
2024-02-03 20:14:48 +0300

    adding, deleting, updating through apis

--- CoreProjectApi/Controllers/CategoryController.cs
+++ CoreProjectApi/Controllers/CategoryController.cs
@@ -1,7 +1,9 @@
 ﻿using CoreProjectApi.DAL.ApiContext;
+using CoreProjectApi.DAL.ApiContext.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CoreProjectApi.Controllers
 {
@@ -16,17 +18,58 @@ namespace CoreProjectApi.Controllers
             return Ok(c.Categories2.ToList()); //apilerde sonucun başarılı olduğunu gösterir
         }
         [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        public IActionResult CategoryGet(int id)
         {
             using var c = new Context();
             var value = c.Categories2.Find(id);
-            if(value == null)
+            if (value == null)
             {
                 return NotFound(); //hiçbir şey bulunamadı döndür
             }
             else
             {
                 return Ok(value);
+            }
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category2 p)
+        {
+            using var c = new Context();
+            c.Add(p);
+            c.SaveChanges();
+            return Created("", p);// geriye oluşturuldu metodu döndür
+        }
+        [HttpDelete]
+        public IActionResult CategoryDelete(int id)
+        {
+            using var c = new Context();
+            var value = c.Categories2.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Remove(value);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category2 p)
+        {
+            using var c = new Context();
+            var value = c.Find<Category2>(p.Category2ID);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                value.Category2Name = p.Category2Name;
+                c.Update(value);
+                c.SaveChanges();
+                return NoContent();
             }
         }
     }
