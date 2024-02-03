7f6d65cbc6a7440fc1ad313f3b53a7f4c99e1e26
Melike İsbir <melikeisbr@gmail.com>
2024-02-03 20:01:42 +0300

    making get requests with apis making get requests by id in apis

---
+++ CoreProjectApi/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using CoreProjectApi.DAL.ApiContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreProjectApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var c = new Context();
+            return Ok(c.Categories2.ToList()); //apilerde sonucun başarılı olduğunu gösterir
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using var c = new Context();
+            var value = c.Categories2.Find(id);
+            if(value == null)
+            {
+                return NotFound(); //hiçbir şey bulunamadı döndür
+            }
+            else
+            {
+                return Ok(value);
+            }
+        }
+    }
+}
