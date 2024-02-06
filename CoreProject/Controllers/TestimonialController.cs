9299c1e3f722c207205f1e4917b9019021d1f022
Melike İsbir <melikeisbr@gmail.com>
2024-02-06 19:35:20 +0300

    testimonial modifications, add testimonial

--- CoreProject/Controllers/TestimonialController.cs
+++ CoreProject/Controllers/TestimonialController.cs
@@ -13,6 +13,17 @@ namespace CoreProject.Controllers
             var values = testimonialManager.TGetList();
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial p)
+        {
+            testimonialManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
         public IActionResult DeleteTestimonial(int id)
         {
             var values = testimonialManager.TGetByID(id);
