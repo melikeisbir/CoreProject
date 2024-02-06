36563e95f62eefd2e843bb5d997180e85bf5f130
Melike İsbir <melikeisbr@gmail.com>
2024-02-06 19:20:08 +0300

    admin dashboard modifications

--- CoreProject/ViewComponents/Dashboard/MessageList.cs
+++ CoreProject/ViewComponents/Dashboard/MessageList.cs
@@ -1,14 +1,17 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreProject.ViewComponents.Dashboard
 {
     public class MessageList : ViewComponent
     {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = messageManager.TGetList().Take(5).ToList();
+            return View(values);
         }
     }
 }
